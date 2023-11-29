58e6c71a0864e6633c33e21b7311c90f4f52556a
itskellyne <kellyne333@gmail.com>
2023-11-29 11:43:34 -0500

    class work

--- 0-basic-examples/index.js
+++ 0-basic-examples/index.js
@@ -1,11 +1,14 @@
 // a generic event handler
-const eventHandler = (event) => console.log(event);
+const eventHandler = (event) => console.log(event.target);
 
 // register an event listener on a button
-const button = document.querySelector('button')
+const button = document.querySelector('button');
 const mouseArea = document.querySelector('#mouse-area');
 
 // TODO:
 // add a 'click' handler for the button
+button.addEventListener('click', eventHandler)
 // add a 'mousemove' handler for the mouseArea
+mouseArea.addEventListener('mousemove', eventHandler)
 // add a 'keydown' handler for the entire document
+document.body.addEventListener('keydown', eventHandler)
